3b61e34d1bb98ff32402b785c3ba5104ddb0b569
emmanueladdo <asememmanueladdo@gmail.com>
2023-06-27 02:31:30 -0700

    shell sort

---
+++ 100-shell_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+* shell_sort - sorts an array of integers using
+* shell sort algorithm with Knuth sequence
+* @array: the array of integers
+* @size: size of the array
+*
+* Return: none
+*/
+void shell_sort(int *array, size_t size)
+{
+	size_t gap = 0, j, i;
+	int temp;
+
+	if (size < 2)
+		return;
+
+	while ((gap = gap * 3 + 1) < size)
+		;
+
+	gap = (gap - 1) / 3;
+
+	for (; gap > 0; gap = (gap - 1) / 3)
+	{
+		for (i = gap; i < size; i++)
+		{
+			temp = array[i];
+			for (j = i; j >= gap && temp <= array[j - gap]; j -= gap)
+				array[j] = array[j - gap];
+			array[j] = temp;
+		}
+		print_array(array, size);
+	}
+}
