6772dc59a3ef0ecbd96503186955859fc77f8e1d
emmanueladdo <asememmanueladdo@gmail.com>
2023-06-27 02:11:33 -0700

    insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,46 +1,52 @@
 #include "sort.h"
+
 /**
- * _swap_list - help sort doubly-linked list.
- *@head: head of the list.
- *@node1: first node to be swapped/sorted.
- *@node2: second node to be swapped/sorted.
- * Return: void.
+ * _swap - swaps nodes in a doubly-linked list
+ * @a: addr of first node
+ * @b: addr of second node
+ * Return: void
  */
-void _swap_list(listint_t **head, listint_t **node1, listint_t *node2)
+void _swap(listint_t *a, listint_t *b)
 {
-	(*node1)->next = node2->next;
-	if (node2->next != NULL)
-		node2->next->prev = *node1;
-	node2->prev = (*node1)->prev;
-	node2->next = *node1;
-	if ((*node1)->prev != NULL)
-		(*node1)->prev->next = node2;
-	else
-		*head = node2;
-	(*node1)->prev = node2;
-	*node1 = node2->prev;
+	if (a->prev)
+		a->prev->next = b;
+	if (b->next)
+		b->next->prev = a;
+	a->next = b->next;
+	b->prev = a->prev;
+	a->prev = b;
+	b->next = a;
+
 }
 
 /**
- * insertion_sort_list - sorts a double linked list using insertion method.
- *@list: list to be sorted.
- * Return: void.
+ * insertion_sort_list - insertion sorts a doubly-linked list
+ * @list: address of pointer to head node
+ * Return: void
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *i, *insert, *temp;
+	listint_t *i, *j;
 
-	if (list == NULL || *list == NULL)
+	if (!list || !*list || !(*list)->next)
 		return;
-
-	for (i = (*list)->next; i != NULL; i = temp)
+	i = (*list)->next;
+	while (i)
 	{
-		temp = i->next;
-		insert = i->prev;
-		while (insert != NULL && i->n < insert->n)
+		j = i;
+		i = i->next;
+		while (j && j->prev)
 		{
-			swap_list(list, &insert, i);
-			print_list((const listint_t *)*list);
+			if (j->prev->n > j->n)
+			{
+				_swap(j->prev, j);
+				if (!j->prev)
+					*list = j;
+				print_list((const listint_t *)*list);
+			}
+			else
+				j = j->prev;
 		}
+
 	}
 }
